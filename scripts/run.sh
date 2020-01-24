7d528cc1c2721bf54dff83a3571c01b0b3b1d03a
Alex Macdonald <almacdon@redhat.com>
2020-01-24 10:16:22 -0500

    fix: disable uitests for windows build

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,4 +4,4 @@
 bash $(dirname "$0")/1_setup_jmc.sh || { echo "Failed to build JMC."; exit 1; };
 
 # Run the ui tests
-bash $(dirname "$0")/3_run_ui_tests.sh || { echo "Failed to run UI tests."; exit 1; };
+# bash $(dirname "$0")/3_run_ui_tests.sh || { echo "Failed to run UI tests."; exit 1; };
