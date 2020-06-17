e9b4e35c3c397eb8a8193fc342a951bc6f6c812a
Alex Macdonald <almacdon@redhat.com>
2020-06-17 14:12:35 -0400

    jfr tests using OracleJDK 11

--- scripts/1_setup_jmc.sh
+++ scripts/1_setup_jmc.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 source $(dirname "$0")/config.sh
 
-# if a jmc folder already exists, remove it prior to cloning
+# # if a jmc folder already exists, remove it prior to cloning
 if [ -d $JMC_ROOT ]; then
     rm -rf $JMC_ROOT;
 fi
 
 # clone the jmc repo
 git clone $JMC_REPO $JMC_ROOT || { echo $CLONE_ERROR; exit 1; };
+
+# apply the local patch
+cd $JMC_ROOT;
+git apply ../patches/0-run-jfr.patch;
+cd ..;
 
 # setup the p2 repository
 mvn p2:site -f $JMC_THIRD_PARTY/pom.xml || { echo $P2_ERROR; exit 1; };
